314a88e619b1942bf009f08f099dcd2163fc2e52
Rafael Sousa <Rafael Sousa@DESKTOP-CLOKH26>
2019-02-24 13:48:43 -0300

    Criação da página inicial e de calculo de juros simples.

---
+++ POO - PROJETO1/src/java/br/com/fatecpg/PooProjeto1/JurosSimples.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.PooProjeto1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Rafael Sousa
+ */
+@WebServlet(name = "JurosSimples", urlPatterns = {"/juros-simples.html"})
+public class JurosSimples extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+                out.println("<head>");
+                    out.println("<title>Juros Simples </title>");   
+                    
+                out.println("</head>");
+                out.println("<body>");
+                
+                    out.println("<div class='geral'>");
+                        out.println("<h1>Juros Simples</h1>");
+
+                        out.println("<hr/>");
+                        
+                        out.println("<h3 style='color: darkblue;'>J = C . i . t</h3>");
+                        out.println("<h3 style='color: darkblue;'>M = J + C</h3>");
+                        out.println("<h3>J = juros <br/>M = Montante<br/>C = Capital <br/>i = Taxa de Juros <br/>t = Período</h3></div>");
+                      
+                        out.println("<form class='formulario'>");
+                            out.println("<b>Capital</b><input type='text' name='C' />");
+                            out.println("<b>Taxa</b><input type='text' name='i'/>");
+                            out.println("<b>Período</b><input type='text' name='t'/>");
+                            out.println("<input class='botao' type='submit' name='calcular' value='Calcular'/>");
+                        
+                            if (request.getParameter("calcular") != null) {
+                            try {
+                                double C = Double.parseDouble(request.getParameter("C"));
+                                double i = Double.parseDouble(request.getParameter("i"));
+                                double t = Double.parseDouble(request.getParameter("t"));
+
+                                 double J = (C * (i / 100) * t);
+                                 double M = J + C;
+
+                                 out.println("<p class='resposta'>Juros de R$  "+J+"</p>");
+                                 out.println("<p class='resposta'>Montante de R$  "+M+"</p>");
+                                         
+                            } catch (NumberFormatException ex) {
+                                out.println("<h1 style='color:red';>Erro ao converter os campos</h1>");
+                            } 
+                        }
+                       out.println("<br/>");
+                       out.println("<button style='margin-top: 20px;'><a href='index.html'>Voltar</a></button>"); 
+                       out.println("</form>");                                                                                                            
+                out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
